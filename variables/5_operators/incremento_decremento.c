df527ed5bc8a281f4087acd380d01928cca53854
Edgar Daniel García García <danielgg1103@gmail.com>
2021-03-11 14:54:06 -0600

    Update incremento_decremento.c

--- variables/5_operators/incremento_decremento.c
+++ variables/5_operators/incremento_decremento.c
@@ -3,24 +3,24 @@
 float a,b;
 int main()
 { // Inicio
- system("clear"); // Limpia pantallpa
-	printf("Dame el primer número\n");
+ 
+	printf("Dame el primer numero\n");
 	scanf("%f",&a);
  printf("Valor inicial de a = %f\n",a);
  b=++a;
  printf("Valor de b en el momento de hacer la operacion ++a = %f\n",b);
- printf("Valor de a después de hacer ++a = %f\n",a);
+ printf("Valor de a despues de hacer ++a = %f\n",a);
  printf("Valor inicial de a = %f\n",a);
  b=a++;
- printf("Valor de b en el momento de hacer la operación a++ = %f\n",b);
- printf("Valor de a después de hacer a++ = %f\n",a);
+ printf("Valor de b en el momento de hacer la operacion a++ = %f\n",b);
+ printf("Valor de a despues de hacer a++ = %f\n",a);
  printf("Valor inicial de a = %f\n",a);
  b=--a;
- printf("Valor de b en el momento de hacer la operación --a = %f\n",b);
- printf("Valor de a después de hacer --a = %f\n",a);
+ printf("Valor de b en el momento de hacer la operacion --a = %f\n",b);
+ printf("Valor de a despues de hacer --a = %f\n",a);
  printf("Valor inicial de a = %f\n",a);
  b=a--;
- printf("Valor de b en el momento de hacer la operación a-- = %f\n",b);
- printf("Valor de a después de hacer a-- = %f\n",a);
+ printf("Valor de b en el momento de hacer la operacion a-- = %f\n",b);
+ printf("Valor de a despues de hacer a-- = %f\n",a);
  return 0;
 } // Fin de programa
